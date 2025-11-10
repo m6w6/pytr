e7b5d88f416ee0e431fb79c27278c0f0c4956058
Michael Wallner <mike@php.net>
2025-11-10 17:00:53 +0100

    Add new event type for Ausschüttung

--- pytr/event.py
+++ pytr/event.py
@@ -128,6 +128,7 @@ subtitle_event_type_mapping = {
     "Verkaufsorder": ConditionalEventType.TRADE_INVOICE,
     "Sparplan ausgeführt": ConditionalEventType.TRADE_INVOICE,
     "Tilgung": ConditionalEventType.TRADE_INVOICE,
+    "Ausschüttung / Ausschüttungsgleicher Ertrag": ConditionalEventType.TRADE_INVOICE,
 }
 
 events_known_ignored = [
